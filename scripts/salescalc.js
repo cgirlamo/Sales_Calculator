4d04685372313b820a40dd33b4865b68092a1d0a
cgirlamo <cgirlamo@unm.edu>
2021-02-06 13:37:10 -0700

    Second Prototype sales calc. Fixes #2

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -1,9 +1,9 @@
 addEventListener("load", main)
-
+// This fucntion will run the calculateSales function once a feature is clicked
 function main() {
     btCalcSales.addEventListener("click", calculateSales)
 }
-
+//This function calculates the sales of a clicked upon object 
 function calculateSales() {
     Price=parseFloat(txPrice.value)
     TaxRate=parseFloat(txTaxRate.value)/100
